fd8864abda3877372348c8b7ab9f90eee22094a2
ricardo55991 <ricardo.barros.jr@gmail.com>
2023-07-27 14:52:08 -0300

    Add nova biblioteca para transformar html para pdf

--- src/servico-pagamento/servico-pagamento/Core/HtmlParaPdf.cs
+++ src/servico-pagamento/servico-pagamento/Core/HtmlParaPdf.cs
@@ -1,16 +1,36 @@
 ﻿using servico_pagamento.Core.Interface;
+using WkHtmlToPdfDotNet;
+using WkHtmlToPdfDotNet.Contracts;
 
 namespace servico_pagamento.Core
 {
     public class HtmlParaPdf : IHtmlParaPdf
     {
+        private readonly IConverter _converterPdf;
+
+        public HtmlParaPdf(IConverter converterPdf)
+        {
+            _converterPdf = converterPdf;
+        }
+
         public byte[] ConverterHtmlParaPdf(string htmlContent)
         {
-            var renderer = new ChromePdfRenderer();
+            var doc = new HtmlToPdfDocument()
+            {
+                GlobalSettings = {
+                    ColorMode = ColorMode.Color,
+                    Orientation = Orientation.Portrait,
+                    PaperSize = PaperKind.A4
+                },
+                Objects = {
+                    new ObjectSettings() {
+                        HtmlContent = htmlContent,
+                        WebSettings= { DefaultEncoding = "utf-8"}
+                    }
+                }
+            };
 
-            var pdfDocument = renderer.RenderHtmlAsPdf(htmlContent);
-
-            return pdfDocument.Stream.ToArray();
+            return _converterPdf.Convert(doc);
         }
     }
 }
